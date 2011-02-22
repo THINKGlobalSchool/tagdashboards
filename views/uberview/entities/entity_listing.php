d53f0deb9582d917ca0444467d14c23d03a289f2
Jeff Tilson <jrtilson@gmail.com>
2011-02-22 16:52:29 +0000

    Added entities/entity_listing and profile/icon (just includes the default version of those views)

---
+++ views/uberview/entities/entity_listing.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Ubertags entity listing view (just uses the default view)
+ *
+ * @package Ubertags
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Jeff Tilson
+ * @copyright THINK Global School 2010
+ * @link http://www.thinkglobalschool.com/
+ */
+echo elgg_view('entities/entity_listing', $vars, FALSE, FALSE, 'default');
---
+++ views/uberview/profile/icon.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Ubertags profile listing view (just uses the default view)
+ *
+ * @package Ubertags
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Jeff Tilson
+ * @copyright THINK Global School 2010
+ * @link http://www.thinkglobalschool.com/
+ */
+echo elgg_view('profile/icon', $vars, FALSE, FALSE, 'default');
