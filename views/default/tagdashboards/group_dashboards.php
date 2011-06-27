a017e9d648a093a91421dd739b1666e2d90b1bc8
Jeff Tilson <jrtilson@gmail.com>
2011-06-27 19:31:53 +0000

    Updating group module to use new groups/profile/module view

--- views/default/tagdashboards/group_dashboards.php
+++ views/default/tagdashboards/group_dashboards.php
@@ -21,9 +21,6 @@ $all_link = elgg_view('output/url', array(
 	'text' => elgg_echo('link:view:all'),
 ));
 
-$header = "<span class=\"groups-widget-viewall\">$all_link</span>";
-$header .= '<h3>' . elgg_echo('tagdashboards:label:grouptags') . '</h3>';
-
 elgg_push_context('widgets');
 $options = array(
 	'type' => 'object',
@@ -40,12 +37,15 @@ if (!$content) {
 	$content = '<p>' . elgg_echo('tagdashboards:none') . '</p>';
 }
 
-if ($group->canWriteToContainer()) {
-	$new_link = elgg_view('output/url', array(
-		'href' => "tagdashboards/add/$group->guid",
-		'text' => elgg_echo('tagdashboards:add'),
-	));
-	$content .= "<span class='elgg-widget-more'>$new_link</span>";
-}
+$new_link = elgg_view('output/url', array(
+	'href' => "tagdashboards/add/$group->guid",
+	'text' => elgg_echo('tagdashboards:add'),
+));
 
-echo elgg_view_module('info', '', $content, array('header' => $header));
+echo elgg_view('groups/profile/module', array(
+	'title' => elgg_echo('tagdashboards:label:grouptags'),
+	'content' => $content,
+	'all_link' => $all_link,
+	'add_link' => $new_link,
+));
+
