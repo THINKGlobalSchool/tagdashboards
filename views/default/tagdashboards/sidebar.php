3c56219ff0a1fca9473bf29942621ef4c8775243
Jeff Tilson <jrtilson@gmail.com>
2013-07-10 21:21:25 +0000

    Only display featured tagdashboards in 'all' context

--- views/default/tagdashboards/sidebar.php
+++ views/default/tagdashboards/sidebar.php
@@ -10,22 +10,24 @@
  * 
  */
 
-$featured_dashboards = elgg_get_entities_from_metadata(array(
-	'metadata_name' => 'featured_dashboard',
-	'metadata_value' => 'yes',
-	'type' => 'object',
-	'subtype' => 'tagdashboard',
-	'limit' => 10,
-));
+if (!elgg_get_page_owner_guid()) {
+	$featured_dashboards = elgg_get_entities_from_metadata(array(
+		'metadata_name' => 'featured_dashboard',
+		'metadata_value' => 'yes',
+		'type' => 'object',
+		'subtype' => 'tagdashboard',
+		'limit' => 10,
+	));
 
-if ($featured_dashboards) {
+	if ($featured_dashboards) {
 
-	elgg_push_context('widgets');
-	$body = '';
-	foreach ($featured_dashboards as $dashboard) {
-		$body .= elgg_view_entity($dashboard, array('full_view' => false));
+		elgg_push_context('widgets');
+		$body = '';
+		foreach ($featured_dashboards as $dashboard) {
+			$body .= elgg_view_entity($dashboard, array('full_view' => false));
+		}
+		elgg_pop_context();
+
+		echo elgg_view_module('aside', elgg_echo("tagdashboards:label:featured"), $body);
 	}
-	elgg_pop_context();
-
-	echo elgg_view_module('aside', elgg_echo("tagdashboards:label:featured"), $body);
 }
